776f728bba2d56a0393890efe51ee9da4fad6dc1
Abdallah Joudeh <46824456+AcePhire@users.noreply.github.com>
2023-07-13 21:27:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $('.contactButton').click(function() {
-      $('#contact div').fadeIn();
-    });
+  $('.contactButton').click(function() {
+    $('#contact div').fadeIn();
   });
+
+  var alterClass = function() {
+  var width = document.body.clientWidth;
+  if(width < 980){
+    $('#extras a').removeClass('large');
+    $('#extras a').removeClass('massive');
+    $('#extras a').addClass('huge');
+  }else{
+    $('#extras a').removeClass('large');
+    $('#extras a').removeClass('huge');
+    $('#extras a').addClass('massive');
+    };
+  };
+
+  $(window).resize(function(){
+    alterClass();
+  });
+
+  alterClass();
+});
