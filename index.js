dca2a1d2f025768aa2b1c0ecb715fb238becbc37
Abdallah Joudeh <46824456+AcePhire@users.noreply.github.com>
2023-07-13 22:10:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  $introduction = $("#introduction");
+  $skills = $("#skills");
+  $extras = $("#extras");
+  $hr = $("hr")
+  $theMenu = $("#menu");
+  $nav = $("#navigation-bar");
+
   $('.contactButton').click(function() {
     $('#contact div').fadeIn();
   });
@@ -21,4 +28,18 @@ $(document).ready(function() {
   });
 
   alterClass();
+
+  $("#navigation-bar .menu-button").on("click", function(){
+    $theMenu.animate({width: "toggle"}, 340, function(){});
+  });
+
+  $("#menu .menu-button").on("click", function(){
+    $introduction.css("display", "flex");
+    $skills.css("display", "block");
+    $extras.css("display", "flex");
+    $hr.css("display", "block");
+
+    $nav.css({width: "100vw"});
+    $theMenu.animate({width: "toggle"}, 340);
+  });
 });
